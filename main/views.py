e402accb1de24199f146544caa9f9fa9d7cb0a38
Brett Chalupa <brettchalupa@gmail.com>
2014-12-03 14:45:45 -0500

    Add content, adjust styles, and set settings
    This creates the initial version of this page, which displays an embedded iframe
    of an interactive timeline.

--- main/views.py
+++ main/views.py
@@ -5,17 +5,17 @@ from config import FREEZER_BASE_URL
 
 @app.route('/')
 def index():
-    page_title = 'VPR App Template'
+    page_title = "Timeline: A Year Of 'Systemic Failure' At DCF"
     page_url = FREEZER_BASE_URL.rstrip('/') + request.path
 
     social = {
-        'title': "",
-        'subtitle': "",
-        'img': "",
-        'description': "",
-        'twitter_text': "",
-        'creator': "",
-        'twitter_hashtag': ""
+        'title': "Timeline: A Year Of 'Systemic Failure' At DCF",
+        'subtitle': "This year has brought a series of tragedies and revelations about problems at Vermont's Department for Children and Families. VPR looks back over the months.",
+        'img': "http://www.vpr.net/apps/timeline-dcf-systemic-failure/static/img/timeline-social-snap.png",
+        'description': "This year has brought a series of tragedies and revelations about problems at Vermont's Department for Children and Families. VPR looks back over the months.",
+        'twitter_text': "Timeline: A Year Of 'Systemic Failure' At DCF",
+        'creator': "Taylor Dobbs and Angela Evancie",
+        'twitter_hashtag': "VT"
     }
 
     return render_template('content.html',
